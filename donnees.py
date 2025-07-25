6337ea90b1f886900b5e804df05f86de74dacac0
Oumar <oumardiakite476@gmail.com>
2025-07-25 17:42:22 +0200

    Premier commit

---
+++ donnees.py
@@ -0,0 +1,11 @@
+#---------donnees
+Liste_des_mots = ["Ville", "Problème", "Inventer", "Famille", "Fonction","Buisson","Bureau", "Garage"] #Liste des mots du pendu
+
+
+nb_coups = 8
+#Nom du fichier stockant les scores
+nom_fichier_scores = "scores"
+lettre_entree = ""
+#chaine_lettre_entree="" #ne pas oublier de le convertir en minuscule mais aussi chaque élément de liste_lettres_ordi
+liste_1 = ["*","*","*","*","*","*","*","*"] #liste dont la modification se fait en fonction des lettres trouvées, les étoiles étant remplacées par les bonnes lettres
+chaine_liste_1 = "".join(liste_1)#Mot à afficher avec les lettres trouvées
